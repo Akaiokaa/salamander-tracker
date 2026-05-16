effe9766fdab84293e47088f97f5695316ae8e5b
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-16 15:08:15 -0700

    Added Rendering for each video file

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,16 +1,18 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getVideos } from '../mockApi.js';
 
 function Videos(){
+    const [videos, setVideos] = useState([])
     useEffect(() => {
-        getVideos().then((data) => {
-            console.log("getVideos returned:", data);
-        });
+        getVideos().then((data) => setVideos(data));
     }, []);
 
     return (
         <div className="videos-section">
             <h1>Available Videos</h1>
+            {videos.map((videoName) => (
+                <li key={videoName}>{videoName}</li>
+            ))}
             <p>Video list will go here.</p>
         </div>
     )

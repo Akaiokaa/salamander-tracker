204abab56852146d73527951d44e860b43467967
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-16 15:02:30 -0700

    UseEffect and Display videos in console

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,13 @@
+import { useEffect } from 'react';
+import { getVideos } from '../mockApi.js';
 
 function Videos(){
+    useEffect(() => {
+        getVideos().then((data) => {
+            console.log("getVideos returned:", data);
+        });
+    }, []);
+
     return (
         <div className="videos-section">
             <h1>Available Videos</h1>

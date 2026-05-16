9a68965db377c5ebcc40ed2c6ce42ee105188dfd
Akai <161771111+Akaiokaa@users.noreply.github.com>
2026-05-16 14:47:47 -0700

    Navbar with page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home.jsx'
 import Videos from './components/Videos.jsx'
+import Navbar from './components/Navbar.jsx'
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/videos" element={<Videos />}/>
-    </Routes>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/videos" element={<Videos />}/>
+      </Routes>
+    </div>
   )
 }
 

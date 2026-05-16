6d94cbf00c8ca1215b4775979226d679588d6fe5
allenres <allen.resulidze@gmail.com>
2026-05-16 16:57:25 -0700

    test tailwind, cleanup files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
-function Home(){
+function Home() {
     return (
         <div className="home-page">
-            <h1>Salamder Tracker</h1>
+            <h1 className="text-4xl font-bold text-blue-600">Salamander Tracker</h1>
             <p>Pick a video from the videos page to start analyzing.</p>
         </div>
     )
